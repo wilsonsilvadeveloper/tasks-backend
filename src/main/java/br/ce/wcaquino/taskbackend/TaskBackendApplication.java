04ecbc165f1890bf1af66e48fae6c831d8252eb9
Wilson <wilsonoficial.com@gmail.com>
2025-04-17 11:49:32 -0300

    Ajusta a formatação do código na classe TaskBackendApplication

--- src/main/java/br/ce/wcaquino/taskbackend/TaskBackendApplication.java
+++ src/main/java/br/ce/wcaquino/taskbackend/TaskBackendApplication.java
@@ -6,14 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TaskBackendApplication extends SpringBootServletInitializer {
+public class TaskBackendApplication  extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TaskBackendApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TaskBackendApplication.class);
+    }
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TaskBackendApplication.class);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TaskBackendApplication.class, args);
+    }
 }
